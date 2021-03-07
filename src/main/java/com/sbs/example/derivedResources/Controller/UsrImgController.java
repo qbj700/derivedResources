3d6c741728da87eef31be0ebdd66e9b8bc9e1870
qbj700 <qbj700@gmail.com>
2021-03-07 18:01:44 +0900

    요청된 파생을 기억한 후, 첫 요청이면 원본파일 저장

--- src/main/java/com/sbs/example/derivedResources/Controller/UsrImgController.java
+++ src/main/java/com/sbs/example/derivedResources/Controller/UsrImgController.java
@@ -4,11 +4,15 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sbs.example.derivedResources.Service.DeriveRequestService;
+import com.sbs.example.derivedResources.Service.GenFileService;
+import com.sbs.example.derivedResources.dto.DeriveRequest;
 import com.sbs.example.derivedResources.util.Util;
 
 @RestController
@@ -16,13 +20,29 @@ public class UsrImgController {
 	@Value("${custom.tmpDirPath}")
 	private String tmpDirPath;
 	
+	@Autowired
+	private GenFileService genFileService;
+	@Autowired
+	private DeriveRequestService deriveRequestService;
+
 	@RequestMapping("/img")
-	public String showImg(HttpServletRequest req, @RequestParam Map<String, Object> param) {
+	public DeriveRequest showImg(HttpServletRequest req, @RequestParam Map<String, Object> param) {
 		String queryString = req.getQueryString();
 		String url = queryString.split("url=")[1];
 
-		String filePath = Util.downloadFileByHttp(url, tmpDirPath);
+		DeriveRequest deriveRequest = deriveRequestService.getDeriveRequestByUrl(url);
 
-		return filePath;
+		if (deriveRequest == null) {
+			int width = Util.getAsInt(param.get("width"), 0);
+			int height = Util.getAsInt(param.get("height"), 0);
+			int maxWidth = Util.getAsInt(param.get("maxWidth"), 0);
+			String filePath = Util.downloadFileByHttp(url, tmpDirPath);
+
+			deriveRequestService.save(url, width, height, maxWidth, filePath);
+		}
+
+		deriveRequest = deriveRequestService.getDeriveRequestByUrl(url);
+
+		return deriveRequest;
 	}
 }
