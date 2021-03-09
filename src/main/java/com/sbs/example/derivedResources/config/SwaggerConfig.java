0b39c1336e826622862d73a1677babfaa445fc2e
qbj700 <qbj700@gmail.com>
2021-03-09 15:44:44 +0900

    swagger 메타 정보 입력

--- src/main/java/com/sbs/example/derivedResources/config/SwaggerConfig.java
+++ src/main/java/com/sbs/example/derivedResources/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.sbs.example.derivedResources.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +22,12 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.sbs.example.derivedResources.controller"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(apiInfo());
+	}
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("이미지 파생 서비스").description("간단한 imgURL을 통해 파생된 이미지를 제공합니다.")
+				.contact(new Contact("제작자", "https://github.com/qbj700/derivedResources", "qbj700@gmail.com"))
+				.version("0.1").build();
 	}
 } 
